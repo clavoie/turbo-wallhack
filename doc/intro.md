917c668262934318a1af41573bccc2b5da60e0d9
clavoie <clavoie@gmail.com>
2015-05-11 22:12:03 -0400

    lein template for project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to turbo-wallhack
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/turbo_wallhack/core_test.clj
@@ -0,0 +1,7 @@
+(ns turbo-wallhack.core-test
+  (:require [clojure.test :refer :all]
+            [turbo-wallhack.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
