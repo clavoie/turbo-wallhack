2f8fce0183321e282a929129869c7764552181bb
clavoie <clavoie@gmail.com>
2015-05-11 22:57:06 -0400

    WIP. nothing yet just banging around ideas

--- src/turbo_wallhack/core.clj
+++ src/turbo_wallhack/core.clj
@@ -1,6 +1,36 @@
 (ns turbo-wallhack.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create
+  "Creates a new turbo wallhack"
+  []
+  (sorted-map))
+
+;; (add index "hey" {pod})
+
+;; need to tokenize
+
+(defn add-value [index items value]
+  (let [items (seq items)]
+    (loop [index index
+           vindexes (range 0 (count items))]
+      (if (empty? items)
+        index
+        (recur (merge index )
+               (rest vindexes))))))
+
+
+(defn add-top-keys [index items]
+  (loop [index index
+         items (seq items)]
+    (if (empty? items)
+      index
+      (recur (merge {(first item) []} index)
+             (rest items)))))
+
+(defn add
+  "Adds an item to a search term in the wallhack"
+  [index items value]
+  (let [index (add-top-keys index items)
+        ;; index (add-value items value)
+        ]
+    index))
