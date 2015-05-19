ac8f47590243f29619414c1190cda2e72b7b699c
unknown <clavoie@draftkings.com>
2015-05-19 16:31:38 -0400

    Added unit tests for the main node interface. fixed some bugs

--- src/turbo_wallhack/node.clj
+++ src/turbo_wallhack/node.clj
@@ -1,51 +1,15 @@
-(ns turbo-wallhack.node)
-
-;;
-;; private
-;;
-
-(defn create
-  "Creates a new node in the radix tree"
-  [index]
-  {:index index :edges {} :values nil})
-
-(defn ensure-path
-  "Ensures a path exists or creates it"
-  [node token]
-  (let [token-seq (seq tokens)]
-    (loop [root node
-           this-token (first token-seq)
-           rest-tokens (rest token-seq)
-           path [:edges this-token]]
-      (if-not this-token
-        root
-        (recur (if (get-in root path) root (update-in root path (create this-token)))
-               (first rest-tokens)
-               (rest rest-tokens)
-               (conj path :edges (first this-token)))))))
-
-(defn get-value-path
-  [token]
-  (let [token-seq (seq token)
-        edge-pieces (repeatedly #(identity :edges))
-        path (interleave edge-pieces token-seq)
-        path (take (* 2 (count token)) path)
-        path (vec path)]
-    (conj path :value)))
-
-;;
-;; public
-;;
+(ns turbo-wallhack.node
+  (:require [turbo-wallhack.private.node :as private]))
 
 (defn make-root
   "Create a new root node for a new graph"
   []
-  (create :root))
+  (private/create :root))
 
 (defn add
   "Adds a token to the node index, returns the updated node"
   [node token value]
-  (let [node (ensure-path node token)
+  (let [node (private/ensure-path node token)
         token-seq (seq token)
-        path (get-value-path token)]
+        path (private/get-value-path token)]
     (update-in node path #(conj %1 value))))
---
+++ test/turbo_wallhack/node_test.clj
@@ -0,0 +1,19 @@
+(ns turbo-wallhack.node-test
+  (:require [clojure.test :refer :all]
+            [turbo-wallhack.node :refer :all]))
+
+(deftest make-root-test
+  (let [root (make-root)]
+    (is (map? root))
+    (is (= :root (get root :index)))))
+
+(deftest add-test
+  (let [root (make-root)
+        root (add root "abc" 1)
+        root (add root "abc" 2)
+        root (add root "abd" 3)]
+    (is (empty? (get-in root [:edges \a :values])))
+    (is (empty? (get-in root [:edges \a :edges \b :values])))
+                                ;; private/get-value-path?
+    (is (= '(2 1) (get-in root [:edges \a :edges \b :edges \c :values])))
+    (is (= '(3) (get-in root [:edges \a :edges \b :edges \d :values])))))
